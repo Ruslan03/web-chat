e9a5ff8f155f628b507d801927dfab63613a656b
Ruslan Wanandi <ruslan.wanandi@salt.co.id>
2025-04-19 03:47:45 +0700

    chore: eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
-import Router from "next/router";
 import { useEffect, useState } from "react";
 
 const Home = () => {
